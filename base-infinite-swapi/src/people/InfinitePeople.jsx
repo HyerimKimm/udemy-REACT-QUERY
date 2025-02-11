f8710946632e10bad87361a1810d73422dace87b
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-11 22:18:26 +0900

    Feat. useInfinityQuery Hook 작성

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,28 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = "https://swapi.py4e.com/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
+  /*
+  data: 페이지를 계속 로드할 때 여기에 데이터의 페이지가 포함된다.
+  fetchNextPage: 더 많은 데이터가 필요할 때 어느 함수를 실행할지를 InfiniteScroll에 지시하는 역할
+  hasNextPage: boolean, 수집할 데이터가 있는지를 결정
+   */
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({ pageParam = initialUrl }) => {
+      return fetchUrl(pageParam);
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
   return <InfiniteScroll />;
 }
